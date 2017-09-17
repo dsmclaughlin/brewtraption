b6c3436be0e7ad14fa9c1c21e84821f5e4897642
drew.mclaughlin <drew.mclaughlin@wandisco.com>
2017-09-17 13:20:31 +0100

    update to script to run test server

--- runServer.sh
+++ runServer.sh
@@ -1,6 +1,7 @@
 # Simple script to unpack the server distribution and execue it with no parameters specified
-
+#!/bin/bash
+set -x
 mkdir -p server-dist
-cp target/jetty-jersey-example-dist.tar.gz server-dist
-tar -zxf server-dist/jetty-jersey-example-dist.tar.gz -C server-dist/
-java -jar server-dist/jetty-jersey-example/jetty-jersey-example.jar 
+cp target/brewtraption-dist.tar.gz server-dist
+tar -zxf server-dist/brewtraption-dist.tar.gz -C server-dist/
+java -jar server-dist/brewtraption/brewtraption.jar
